d55621e9ab30657ab3e20979fda4d6bb667dcb06
Deinyefa <df.eporwei@gmail.com>
2021-08-05 14:07:44 -0400

    style nav tabs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 // toggle between percent and round tabs
-function handleTabSelection (sectionID) {
+function handleTabSelection (tabElement, sectionID) {
+    const tabId = tabElement.id;
+    const tabs = document.getElementById("navigation").children;
+
+    console.log(tabs);
+    if (!tabElement.classList.contains("active")) {
+        tabElement.classList.add("active")
+    }
+    [...tabs].forEach(tab => {
+        const anchor = tab.firstChild;
+        if(anchor.id !== tabId) {
+            anchor.classList.remove("active")
+        }
+    })
+
     const sections = document.getElementsByTagName('section');
     let sectionElement = document.getElementById(sectionID);
     
@@ -12,11 +26,11 @@ function handleTabSelection (sectionID) {
         }
     });
 }
-document.getElementById("percent").onclick = (e) => {
+document.getElementById("percent").onclick = function (e){
     e.preventDefault();
-    handleTabSelection("tip-percent")
+    handleTabSelection(this, "tip-percent")
 }
-document.getElementById("round").onclick = (e) => {
+document.getElementById("round").onclick = function (e){
     e.preventDefault();
-    handleTabSelection("tip-round")
+    handleTabSelection(this, "tip-round")
 }
